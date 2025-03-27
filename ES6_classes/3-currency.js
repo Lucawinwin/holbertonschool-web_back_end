7c6556269e17c50814bf3084d698c4f80443eb9c
Lucawinwin <windwehrluca@gmail.com>
2025-03-27 10:50:14 +0100

    ddd

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,60 +1,38 @@
-class Currency {
+export default class Currency {
   constructor(code, name) {
-    // Vérification des types lors de la création de l'objet
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    
     if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
+      throw TypeError('Name must be a string');
     }
-    
-    // Stockage des attributs avec un underscore
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._name = name;
     this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Code must be a string');
+    }
     this._name = name;
   }
-  
-  // Getter pour code
-  get code() {
-    return this._code;
-  }
-  
-  // Setter pour code
-  set code(newCode) {
-    if (typeof newCode !== 'string') {
-      throw new TypeError('Code must be a string');
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
     }
-    this._code = newCode;
+    this._code = code;
   }
-  
-  // Getter pour name
+
   get name() {
     return this._name;
   }
-  
-  // Setter pour name
-  set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
+
+  get code() {
+    return this._code;
   }
-  
-  // Méthode pour afficher la devise complète
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
-}
-
-// Exemple d'utilisation
-try {
-  const dollar = new Currency('USD', 'United States Dollar');
-  console.log(dollar.displayFullCurrency());  // Affiche : United States Dollar (USD)
-  
-  // Modification des attributs
-  dollar.name = 'US Dollar';
-  dollar.code = 'US';
-  console.log(dollar.displayFullCurrency());  // Affiche : US Dollar (US)
-} catch (error) {
-  console.error(error.message);
 }

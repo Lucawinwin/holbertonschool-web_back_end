e2e6040ebb79213e83c85ac8b56b444888efa336
Lucawinwin <windwehrluca@gmail.com>
2025-03-31 12:34:02 +0200

    12h33

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudentIds(listStudents) {
+	if (!Array.isArray(listStudents)) {
+		return[];
+	}
+
+	return listStudents.map((student) => student.id);
+}
+
---
+++ ES6_data_manipulation/9-main.js
@@ -0,0 +1,4 @@
+import groceriesList from "./9-groceries_list.js";
+
+console.log(groceriesList());
+/bin/bash: line 1: q: command not found

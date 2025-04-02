0eaa24f37627e52cbb377ab1ee4f48745d8e15cd
Lucawinwin <windwehrluca@gmail.com>
2025-04-02 11:42:31 +0200

    dd

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,6 @@
-function getListStudentIds(listStudents) {
-	if (!Array.isArray(listStudents)) {
-		return[];
-	}
-
-	return listStudents.map((student) => student.id);
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) return [];
+  return array.map((i) => i.id);
 }
-
+// if arg is not an array, the func is returning an empty array
+// you MUST use map func on the array

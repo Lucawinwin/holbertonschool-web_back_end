82030951868b7a6d37bf95e14ba1801b2ceb9ce7
Lucawinwin <windwehrluca@gmail.com>
2025-04-02 11:46:15 +0200

    dd

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-	return Promise.all([uploadPhoto(), createUser()])
-	.then(([photoResponse, userResponse]) => {
-		console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((tmp) => {
+      console.log(`${tmp[0].body} ${tmp[1].firstName} ${tmp[1].lastName}`);
     })
-	.catch(() => {
-		console.log('Signup system offline');
-	});
+    .catch(() => console.log('Signup system offline'));
 }
+// tmp is computer reponse

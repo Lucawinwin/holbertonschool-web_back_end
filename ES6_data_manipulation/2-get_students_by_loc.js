ae3d26479a3515800160f27aed191c057f8bde67
Lucawinwin <windwehrluca@gmail.com>
2025-04-02 11:37:56 +0200

    dd

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,5 @@
-function getStudentsByLocation
+export default function getStudentsByLocation(array, city) {
+  return array.filter((i) => i.location === city);
+}
+// should accept list of students and city as params
+// you MUST use filter func on the array

7d27997dcd524daa9dc59c4b327bb201d7ee4958
Lucawinwin <windwehrluca@gmail.com>
2025-05-22 10:47:14 +0200

    dd

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,28 @@
-const fs = require('fs');
+const Fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
+    Fs.readFile(path, (err, databaseFile) => {
+      if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
-      const lines = data.split('\n').filter((line) => line.length > 0);
-      if (lines.length <= 1) {
-        resolve('Number of students: 0');
-        return;
-      }
 
-      const fields = {};
-      for (let i = 1; i < lines.length; i += 1) {
-        const student = lines[i].split(',');
-        if (fields[student[3]]) {
-          fields[student[3]].push(student[0]);
-        } else {
-          fields[student[3]] = [student[0]];
-        }
-      }
-
-      let output = `Number of students: ${lines.length - 1}`;
-      for (const field in fields) {
-        if (field) {
-          const list = fields[field];
-          output += `\nNumber of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
-        }
-      }
-
-      resolve(output);
+      const databasefile = databaseFile.toString().split('\n');
+      databasefile.shift();
+      const numOfStudents = databasefile.length;
+      const result = [`Number of students: ${numOfStudents}`, [], []];
+      databasefile.forEach((element) => {
+        const resultElement = element.split(',');
+        if (resultElement[3] === 'CS') {
+          result[1].push(resultElement[0]);
+        } else if (resultElement[3] === 'SWE') { result[2].push(resultElement[0]); }
+      });
+      const finalResult = `${result[0]}\nNumber of students in CS: ${result[1].length}. List: ${result[1].toString().split(',').join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].toString().split(',').join(', ')}`;
+      console.log(finalResult);
+      resolve(finalResult);
     });
   });
 }
 
+module.exports = countStudents;
